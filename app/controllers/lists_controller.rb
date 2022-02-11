aac7f5bfa600c7381530b8c84fc1c011030a749c
EC2 Default User <ec2-user@ip-172-31-4-87.ec2.internal>
2022-02-11 03:34:13 +0000

    lesson1-2_finish

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,20 @@
 class ListsController < ApplicationController
   def new
+  #Viewへ渡すためのインスタンス変数にからのModelオブジェクトを生成する。
+   @list = List.new
   end
 
-  def index
+  def create
+    list = List.new(lists_params)
+
+    list.save
+
+    redirect_to '/homes/top'
   end
 
-  def show
-  end
+  private
 
-  def edit
+  def lists_params
+    params.require(:list).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'homes/top'
+  get 'lists/new' => 'lists#new'
+  get 'lists/index' => 'lists#index'
+  get 'lists/show' => 'lists#show'
+  get 'lists/edit' => 'lists#edit'
+  get 'homes/top' => 'homes#top'
+  post 'lists' => 'lists#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
